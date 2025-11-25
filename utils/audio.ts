1a1c35da2d774de3073d69109cea7af3304dee2b
Kota Ouchi <k0ta0uchi@gmail.com>
2025-11-25 13:15:50 +0900

    feat: Enhance audio tracks with extended lengths
    Refactor audio track data to include longer, looped versions suitable for gameplay. This provides more immersive background music.

--- utils/audio.ts
+++ utils/audio.ts
@@ -14,42 +14,349 @@ interface TrackData {
   drums: string;
 }
 
-// 5 Gothic Horror Tracks (Castlevania-style)
+// 5 Extended Gothic Horror Tracks (Approx 2-3 mins loop)
 const SONGS: Record<number, TrackData> = {
-  // Track 1: The Awakening (Driving, Heroic, Stage 1/6)
+  // Track 1: The Awakening (Driving, Heroic)
+  // Structure: Intro -> A -> A' -> B -> Bridge -> Solo -> A -> Outro -> Loop
   0: {
-    lead: "t150 o4 l8 a a > c c < b b a g f f a a g4 e4 a a > c c < b b a g f e d c b4 < a4",
-    harmony: "t150 o3 l8 f f a a g g f e d d f f e4 c4 f f a a g g f e d c < b a g#4 a4",
-    bass: "t150 o2 l8 a a a a a a a a f f f f e e e e a a a a a a a a f f e e a4 a4",
-    drums: "t150 l4 k s k s k8 k8 s k s"
+    lead: 
+      // Intro
+      "t140 o3 l8 a a a a a a a a a a a a a a a a " +
+      // A Section
+      "o4 a a > c c < b b a g f f a a g4 e4 a a > c c < b b a g f e d c < b4 > c4 " +
+      "a a > c c < b b a g f f a a g4 e4 a a > c c < b b a g f e d c < b4 a4 " +
+      // A' Variation
+      "a a > e e d d c c < b b > d d c4 < a4 > f f a a g g f f e e g g f e d c " +
+      "< a a > c c < b b a g f f a a g4 e4 a a > c c < b b a g f e d c < b4 a4 " +
+      // B Section (Major lift)
+      "f f > c c < a a f f e e > c c < g g e e d d a a f f d d c c e e c4 r4 " +
+      "f f > c c < a a f f g g > d d < b b g g a a > e e c c a a < b b > f f d4 r4 " +
+      // Bridge (Tension)
+      "o4 e e g# g# b b > d d c c < a a f f d d e e g# g# b b > d d e2. r4 " + 
+      // Solo / C Section (Fast runs)
+      "l16 a b > c d e d c < b a > c e a g f e d c < b > c d e f e d c < b > c < b a g# a b > c d " +
+      "e f e d c < b a g# a b > c d e f g a b > c < b a g f e d c < b a g# a4 r4 " +
+      // Return to A
+      "l8 a a > c c < b b a g f f a a g4 e4 a a > c c < b b a g f e d c < b4 a4 " +
+      "a a > c c < b b a g f f a a g4 e4 a a > c c < b b a g f e d c < b4 a4",
+    
+    harmony:
+      // Intro
+      "t140 o3 l8 a a a a a a a a a a a a a a a a " +
+      // A Section
+      "f f a a g g f e d d f f e4 c4 f f a a g g f e d d f f e4 e4 " +
+      "f f a a g g f e d d f f e4 c4 f f a a g g f e d d f f e4 e4 " +
+      // A'
+      "f f > c c < b b a a g g b b a4 f4 d d f f e e d d c c e e d c < b a " +
+      "f f a a g g f e d d f f e4 c4 f f a a g g f e d d f f e4 e4 " +
+      // B Section
+      "o3 a a > f f < c c a a g g > e e < c c g g f f > d d < a a f f e e g g e4 r4 " +
+      "a a > f f < c c a a b b > g g < d d b b c c a a e e c c d d a a f4 r4 " +
+      // Bridge
+      "g# g# b b > d d f f e e c c < a a f f g# g# b b > d d f f e2. r4 " +
+      // Solo Backing
+      "c e a e c e a e d f a f d f a f e g b g e g b g a > c e c < a > c e c " +
+      "d f a f d f a f e g b g e g b g a > c e c < a > c e c < a4 r4 " +
+      // Return A
+      "f f a a g g f e d d f f e4 c4 f f a a g g f e d d f f e4 e4 " +
+      "f f a a g g f e d d f f e4 c4 f f a a g g f e d c < b a g# a",
+      
+    bass:
+      // Intro
+      "t140 o2 l8 a a a a a a a a a a a a a a a a " +
+      // A
+      "f f f f c c c c d d d d a a a a f f f f c c c c d d d d e e e e " +
+      "f f f f c c c c d d d d a a a a f f f f c c c c d d d d e e e e " +
+      // A'
+      "d d d d g g g g c c c c f f f f b b b b e e e e a a a a g g g g " +
+      "f f f f c c c c d d d d a a a a f f f f c c c c d d d d e e e e " +
+      // B
+      "f f f f f f f f c c c c c c c c d d d d d d d d a a a a a a a a " +
+      "f f f f f f f f g g g g g g g g a a a a a a a a d d d d d d d d " +
+      // Bridge
+      "e e e e e e e e a a a a a a a a e e e e e e e e e e e e e e e e " +
+      // Solo
+      "a a a a a a a a d d d d d d d d e e e e e e e e a a a a a a a a " +
+      "d d d d d d d d e e e e e e e e a a a a a a a a a a a a a a a a " +
+      // Return A
+      "f f f f c c c c d d d d a a a a f f f f c c c c d d d d e e e e " +
+      "f f f f c c c c d d d d a a a a f f f f c c c c d d d d a a a a",
+
+    drums:
+      // Intro
+      "t140 l4 k s k s k s k s" +
+      // Main Loop (repeated pattern for length)
+      "l4 k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s " +
+      "k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s " +
+      "k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s " +
+      "k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s " +
+      "k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s " +
+      "k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s " +
+      "k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s " +
+      "k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s k s k s k8 k8 s k s"
   },
-  // Track 2: Clockwork Tower (Mechanical, Arpeggios, Stage 2/7)
+
+  // Track 2: Clockwork Tower (Mechanical, Baroque)
   1: {
-    lead: "t130 o4 l16 c e g > c < g e c e > c < g e c < b > d g > d < g d < b d a c e a e c a c",
-    harmony: "t130 o3 l8 e g > c4 < g4 e4 d g > b4 < g4 d4 c e a4 e4 c4",
-    bass: "t130 o2 l4 c c > c < c g g > g < g a a > a < a",
-    drums: "t130 l16 k k s k k k s k"
+    lead:
+      // Intro (Clock ticking)
+      "t125 o4 l16 c c r c c r c c c c r c c r c c d d r d d r d d d d r d d r d d " +
+      // A Section (Arpeggios)
+      "d f a > d < a f d f c e g > c < g e c e < b > d g > d < g d < b d a > c e a e c a c " +
+      "d f a > d < a f d f c e g > c < g e c e < b > d g > d < g d < b d a > c e a e c a c " +
+      // B Section (Modulation)
+      "f a > c f c < a f a e g > c e c < g e g d f a > d < a f d f c e a > c < a e c e " +
+      "f a > c f c < a f a e g > c e c < g e g d f a > d < a f d f e g# b > e < b g# e g# " +
+      // C Section (High runs)
+      "a > c e a < g > b d g < f a > c f < e g b e d f a > d < c e g > c < b > d g b a > c e a " +
+      "f a > d f < e g > c e < d f a d < c e a c < b > d g > d < a > c f c < g > b d b a > c e a " +
+      // Bridge
+      "g# b > e g# < e b g# e a > c e a < e c a e f# a > d f# < d a f# d g b > d g < d b g d " +
+      // Return A
+      "d f a > d < a f d f c e g > c < g e c e < b > d g > d < g d < b d a > c e a e c a c " +
+      "d f a > d < a f d f c e g > c < g e c e < b > d g > d < g d < b d a > c e a e c a c",
+      
+    harmony:
+      // Intro
+      "t125 o3 l8 r r r r r r r r r r r r r r r r " +
+      // A Section
+      "d f a f c e g e < b > d g d a > c e c " +
+      "d f a f c e g e < b > d g d a > c e c " +
+      // B Section
+      "f a > c < a e g > c < g d f a f c e a e " +
+      "f a > c < a e g > c < g d f a f e g# b g# " +
+      // C Section
+      "a > c e c g b d b f a c a e g b g d f a f c e g e < b > d g d a > c e c " +
+      "f a > c < a e g > c < g d f a f c e a e < b > d g d a > c f c g b > d < b a > c e c " +
+      // Bridge
+      "e g# b g# a > c e c d f# a f# g b > d < b " +
+      // Return A
+      "d f a f c e g e < b > d g d a > c e c " +
+      "d f a f c e g e < b > d g d a > c e c",
+
+    bass:
+      // Intro
+      "t125 o2 l4 d r d r d r d r " +
+      // A
+      "d d c c b b a a d d c c b b a a " +
+      // B
+      "f f e e d d c c f f e e d d e e " +
+      // C
+      "a a g g f f e e d d c c b b a a " +
+      "d d c c b b a a e e f f g g a a " +
+      // Bridge
+      "e e a a d d g g " +
+      // Return A
+      "d d c c b b a a d d c c b b a a",
+      
+    drums:
+      "t125 l16 k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " + // Intro
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " + // A
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " + // A repeat
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " + // B
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " + // B repeat
+      "k s k s k s k s k s k s k s k s k s k s k s k s k s k s k s k s " + // C (faster feel)
+      "k s k s k s k s k s k s k s k s k s k s k s k s k s k s k s k s " + // C repeat
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " + // Bridge
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " + // Return A
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k"  // Return A
   },
-  // Track 3: Spectral Waltz (3/4 feel in 4/4, Spooky, Stage 3/8)
+
+  // Track 3: Spectral Waltz (3/4 feel, Spooky)
   2: {
-    lead: "t160 o4 l4 c e8 g8 > c2 < b a8 g8 a2 g f8 e8 f2 e d8 c8 d2",
-    harmony: "t160 o3 l4 e g8 > c8 < e2 d f8 a8 d2 c e8 g8 c2 < b > d8 g8 < b2",
-    bass: "t160 o2 l4 c > c < c g > g < g f > f < f g > g < g",
-    drums: "t160 l4 k s s k s s"
+    lead:
+      // Intro
+      "t150 o4 l4 e r r e r r e r r e r r " +
+      // A Section
+      "e b8 a8 g2 f c8 < b8 > c2 < b g8 f8 e2 d a8 g8 a2 " +
+      "e b8 a8 g2 f c8 < b8 > c2 < b > d8 c8 < b2 > c < a8 g8 a2 " + 
+      // B Section (Higher)
+      "g > d8 c8 < b2 a > c8 < b8 a2 g b8 a8 g2 f a8 g8 f2 " +
+      "e g8 f8 e2 d f8 e8 d2 c e8 d8 c2 < b > d8 c8 < b2 " +
+      // Music Box Section (High Octave, Staccato)
+      "o5 l8 e g b a c e g b d f a c e2 " +
+      "e g b a c e g b d a c e2 " +
+      // Heavy Section
+      "o3 l4 e b a g f c < b > c < b g f e d a g a " +
+      // Return A
+      "o4 e b8 a8 g2 f c8 < b8 > c2 < b > d8 c8 < b2 > c < a8 g8 a2",
+    
+    harmony:
+      // Intro
+      "t150 o3 l4 r r r r r r r r r r r r " +
+      // A
+      "e g b a c e g b d f a c " +
+      "e g b a c e g b d a c e " + 
+      // B
+      "b > d < g a c f g b e f a d " +
+      "e g b d f a c e g < b > d f " +
+      // Music Box
+      "o4 e g b a c e g b d f a c e r " +
+      "e g b a c e g b d a c e r " +
+      // Heavy
+      "o3 e g b a c e g b d f a c " +
+      // Return A
+      "e g b a c e g b d a c e",
+      
+    bass:
+      // Intro
+      "t150 o2 l4 e e e e e e e e e e e e " +
+      // A
+      "e e e a a a g g g f f f " + 
+      "e e e a a a g g g a a a " +
+      // B
+      "g g g f f f e e e d d d " +
+      "c c c b b b a a a g g g " +
+      // Music Box (Rest)
+      "r r r r r r r r r r r r r r r r " +
+      // Heavy
+      "e e e a a a g g g f f f " +
+      // Return A
+      "e e e a a a g g g a a a",
+
+    drums:
+      // Intro
+      "t150 l4 k s s k s s k s s k s s " +
+      // A
+      "k s s k s s k s s k s s k s s k s s k s s k s s " +
+      "k s s k s s k s s k s s k s s k s s k s s k s s " +
+      // B
+      "k s s k s s k s s k s s k s s k s s k s s k s s " +
+      "k s s k s s k s s k s s k s s k s s k s s k s s " +
+      // Music Box (Light hats)
+      "s s s s s s s s s s s s s s s s s s s s s s s s " +
+      // Heavy
+      "k k s k k s k k s k k s k k s k k s k k s k k s " +
+      // Return A
+      "k s s k s s k s s k s s k s s k s s k s s k s s"
   },
-  // Track 4: Blood Moon (Heavy, Slow, Doom, Stage 4/9)
+
+  // Track 4: Blood Moon (Doom, Heavy, Slow)
   3: {
-    lead: "t100 o3 l8 a > c < a > c < a > c < a > c < b > d < b > d < b > d < b > d < g > b < g > b < g > b < g > b < a > c < a > c < a4 r4",
-    harmony: "t100 o3 l4 a a a a g g g g f f f f e e e e",
-    bass: "t100 o1 l2 a a g g f f e e",
-    drums: "t100 l4 k s k s"
+    lead:
+      // Intro
+      "t90 o3 l2 c g g# g f g c g " +
+      // A Section (Heavy Riff)
+      "l8 c > c < g > c < c > c < g > c < c > c < g > c < c > c < g > c " +
+      "< g# > c < g > c < g# > c < g > c < g# > c < g > c < g# > c < g > c " + 
+      "< f > c < g# > c < f > c < g# > c < g > c < g > c < f > c < f > c " + 
+      "< c > c < g > c < c > c < g > c < c > c < g > c < c4 > c4 " +
+      // B Section (Melodic, higher)
+      "o4 l4 c g f d# d c < g > c " +
+      "d# a# g# g f d# d g " +
+      "c g f d# d c < g > c " +
+      "g# g f d# d c < b > d " +
+      // Bridge (Faster feel, 16ths)
+      "l16 c d# g > c < g d# c < g > c d# g > c < g d# c < g > " +
+      "d f g# > d < g# f d < g# > d f g# > d < g# f d < g# > " +
+      "d# g a# > d# < a# g d# a# > d# g a# > d# < a# g d# a# > " +
+      "f g# > c f < c g# f c > f g# > c f < c g# f c > " +
+      // Return A
+      "l8 o3 c > c < g > c < c > c < g > c < c > c < g > c < c > c < g > c " +
+      "< g# > c < g > c < g# > c < g > c < g# > c < g > c < g# > c < g > c " + 
+      "< f > c < g# > c < f > c < g# > c < g > c < g > c < f > c < f > c " + 
+      "< c > c < g > c < c > c < g > c < c > c < g > c < c4 r4",
+      
+    harmony:
+      // Intro
+      "t90 o3 l2 r r r r r r r r " +
+      // A Section
+      "l4 c c c c g# g# g# g# f f g g c c c c " +
+      "c c c c g# g# g# g# f f g g c c c c " +
+      // B Section
+      "g# g# g# g# a# a# a# a# g# g# g# g# g g g g " +
+      // Bridge
+      "c c c c d d d d d# d# d# d# f f f f " +
+      // Return A
+      "c c c c g# g# g# g# f f g g c c c c",
+      
+    bass:
+      // Intro
+      "t90 o1 l1 c g g# g f g c g " +
+      // A
+      "c g# f g c g# f g " +
+      // B
+      "g# a# g# g g# a# c g " +
+      // Bridge
+      "c d d# f " +
+      // Return A
+      "c g# f g",
+      
+    drums:
+      // Intro
+      "t90 l4 k s k s k s k s k s k s k s k s " +
+      // A
+      "k s k s k s k s k s k s k s k s k s k s k s k s k s k s k s k s " +
+      // B
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " +
+      // Bridge
+      "k s s s k s s s k s s s k s s s " +
+      // Return A
+      "k s k s k s k s k s k s k s k s k s k s k s k s k s k s k s k s"
   },
-  // Track 5: The Castle Keep (Fast, Neo-Classical, Final, Stage 5/10)
+
+  // Track 5: The Castle Keep (Fast, Neo-Classical)
   4: {
-    lead: "t170 o4 l16 a > c e a < g > b d g < f a > c f < e g b e a > c e a < g > b d g < f a > c f < e4",
-    harmony: "t170 o4 l8 c e c e < b > d < b > d < a > c < a > c < g > b < g > b a > c e a",
-    bass: "t170 o2 l8 a a a a g g g g f f f f e e e e a a a a",
-    drums: "t170 l8 k k s k k k s k"
+    lead:
+      // Intro
+      "t170 o4 l16 a > c e a < g > b d g < f a > c f < e g b e a > c e a < g > b d g < f a > c f < e4 " + 
+      // A Section
+      "a > c e a < g > b d g < f a > c f < e g b e c e a > c < b d g > b < a > c e a < g > b e g " +
+      "f a > d f < e g > c e < d f a d < c e a c < b > d g b " +
+      "a > c e a < g > b d g < f a > c f < e g b e a > c e a < g > b d g < f a > c f < e4 " +
+      // B Section (Major)
+      "c e g > c < g e c e < b > d g > d < g d < b d < a > c e a e c a c < g > b d b g d < b g " +
+      "f a > c f c < a f a e g > c e c < g e g d f a > d < a f d f e g# b > e < b g# e g# " +
+      // Solo
+      "a b > c d e d c < b a > c e a g f e d c < b > c d e f e d c < b > c < b a g# a b > c d " +
+      "e f e d c < b a g# a b > c d e f g a b > c < b a g f e d c < b a g# a4 r4 " +
+      // Return A
+      "a > c e a < g > b d g < f a > c f < e g b e a > c e a < g > b d g < f a > c f < e4",
+      
+    harmony:
+      // Intro
+      "t170 o3 l8 a > c e c g b d b f a c a e g b g a > c e c g b d b f a c a e g b g " +
+      // A
+      "a > c e c g b d b f a c a e g b g c e g e d f a f c e g e e g b g " + 
+      "d f a f c e g e d f a f c e a e e g b g " +
+      "a > c e c g b d b f a c a e g b g a > c e c g b d b f a c a e g b g " +
+      // B
+      "c e g e g b d b a > c e c < g b d b f a c a e g b g d f a f e g# b g# " +
+      // Solo Backing
+      "a a a a a a a a f f f f f f f f d d d d d d d d e e e e e e e e " +
+      "f f f f f f f f e e e e e e e e d d d d d d d d e e e e e e e e " +
+      // Return A
+      "a > c e c g b d b f a c a e g b g a > c e c g b d b f a c a e g b g",
+      
+    bass:
+      // Intro
+      "t170 o2 l8 a a a a g g g g f f f f e e e e a a a a g g g g f f f f e e e e " + 
+      // A
+      "a a a a g g g g f f f f e e e e c c c c b b b b a a a a g g g g " +
+      "d d d d c c c c b b b b a a a a e e e e " +
+      "a a a a g g g g f f f f e e e e a a a a g g g g f f f f e e a a " +
+      // B
+      "c c c c b b b b a a a a g g g g f f f f e e e e d d d d e e e e " +
+      // Solo
+      "a a g g f f e e d d c c b b e e a a g g f f e e d d c c b b e e " +
+      // Return A
+      "a a a a g g g g f f f f e e e e a a a a g g g g f f f f e e e e",
+      
+    drums:
+      // Intro
+      "t170 l8 k k s k k k s k k k s k k k s k k k s k k k s k k k s k " +
+      // A
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " +
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " +
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " +
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k " +
+      // B
+      "k s k s k s k s k s k s k s k s k s k s k s k s k s k s k s k s " +
+      // Solo
+      "k k k k s k s k k k k k s k s k k k k k s k s k k k k k s k s k " +
+      "k k k k s k s k k k k k s k s k k k k k s k s k k k k k s k s k " +
+      // Return A
+      "k k s k k k s k k k s k k k s k k k s k k k s k k k s k k k s k"
   }
 };
 
@@ -170,9 +477,12 @@ class MusicEngine {
     this.currentTrack.bass = this.parseMML(trackData.bass);
     this.currentTrack.drums = this.parseMML(trackData.drums);
     
-    // Find loop length (max end time)
+    // Find loop length (max end time of any track)
     const times = [this.currentTrack.lead, this.currentTrack.harmony, this.currentTrack.bass, this.currentTrack.drums]
         .map(t => t.length > 0 ? t[t.length-1].startTime + t[t.length-1].duration : 0);
+    
+    // To ensure seamless loop, we use the max time. 
+    // MML strings should be written to match lengths, but if not, silence will occur on shorter tracks.
     this.loopLength = Math.max(...times);
     
     this.cursors = { lead: 0, harmony: 0, bass: 0, drums: 0 };
@@ -310,6 +620,8 @@ class AudioController {
       this.bgmEnabled = enabled;
       if (!enabled) {
           this.stopMusic();
+      } else if (this.musicEngine['isPlaying'] === false && this.ctx) {
+         // Optionally resume if turning on while game is running, handled by App effect
       }
   }
 
